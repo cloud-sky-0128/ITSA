c65bb22cfc537cf40da0d561ac34e77e008f6be2
cloud-sky-0128 <95736059+cloud-sky-0128@users.noreply.github.com>
2025-06-12 19:12:39 +0800

    Update and rename 薪水計算.c to 薪水計算.cpp

--- ITSA/薪水計算.cpp
+++ ITSA/薪水計算.cpp
@@ -1,10 +1,12 @@
-#include <stdio.h>
+#include <iostream>
+#include <iomanip>
+using namespace std;
 
 int main() {
     int T, S;
     double salary;
 
-    scanf("%d %d", &T, &S);
+    cin >> T >> S;
 
     if (T > 120) {
         salary = S * (60 + 60 * 1.33 + (T - 120) * 1.66);
@@ -14,6 +16,6 @@ int main() {
         salary = S * T;
     }
 
-    printf("%.1f\n", salary);
+    cout << fixed << setprecision(1) << salary << endl;
     return 0;
 }
