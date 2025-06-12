7508a611a40151dd44390b545bbe472313128bd6
cloud-sky-0128 <95736059+cloud-sky-0128@users.noreply.github.com>
2025-06-12 19:04:58 +0800

    Update and rename 十進制轉二進制.c to 十進制轉二進制.cpp

--- ITSA/十進制轉二進制.cpp
+++ ITSA/十進制轉二進制.cpp
@@ -1,21 +1,24 @@
-#include <stdio.h>
+#include <iostream>
+using namespace std;
 
 int main() {
     int N;
     int binary[8] = {0};
 
-    scanf("%d", &N);
+    cin >> N;
 
     if (N < 0) N += 256;
-    
+
     for (int i = 7; i >= 0; i--) {
         binary[i] = N % 2;
         N /= 2;
     }
 
-    for (int i = 0; i < 8; i++) printf("%d", binary[i]);
-    
-    printf("\n");
+    for (int i = 0; i < 8; i++) {
+        cout << binary[i];
+    }
+
+    cout << endl;
 
     return 0;
 }
