7987dbe3d68e2fb267dac9d85bd2e730b378c0a8
cloud-sky-0128 <95736059+cloud-sky-0128@users.noreply.github.com>
2025-06-12 19:18:28 +0800

    Update and rename 計算能被2、3整除，但不能被12整除的整數總和.c to 計算能被2、3整除，但不能被12整除的整數總和.cpp

--- ITSA/計算能被2、3整除，但不能被12整除的整數總和.cpp
+++ ITSA/計算能被2、3整除，但不能被12整除的整數總和.cpp
@@ -1,9 +1,9 @@
-#include <stdio.h>
+#include <iostream>
+using namespace std;
 
 int main() {
     int N, sum = 0;
-
-    scanf("%d", &N);
+    cin >> N;
 
     for (int i = 1; i < N; i++) {
         if (i % 2 == 0 && i % 3 == 0 && i % 12 != 0) {
@@ -11,7 +11,6 @@ int main() {
         }
     }
 
-    printf("%d\n", sum);
-
+    cout << sum << endl;
     return 0;
 }
